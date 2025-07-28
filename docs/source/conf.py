3f34601aaa67ee5ca83e9e8d43ba490a96d83e6c
Allan <aavendan@gmail.com>
2025-07-27 20:19:44 -0500

    styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,9 +54,9 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-#html_css_files = [
-#    'css/style.css',
-#]
+html_css_files = [
+   'css/style.css',
+]
 
 # -- GraphViz configuration ----------------------------------
 graphviz_output_format = 'svg'
